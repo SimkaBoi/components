9e8917bec6c2a4237ea1c445e3dac278301b82fd
SimkaBoi <simkaviiner@gmail.com>
2021-03-09 13:50:07 +0200

    Add navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,48 +6,80 @@
       :clipped="clipped"
       fixed
       app
+      class="grey darken-4"
     >
-      <v-list>
+      <v-list nav dense>
         <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+          v-for="[icon, text] in links_main"
+          :key="icon"
+          link
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          <v-list-item-icon>
+            <v-icon color="grey">{{ icon }}</v-icon>
+          </v-list-item-icon>
+
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>{{ text }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item
+          v-for="[icon, text] in links_secondary"
+          :key="icon"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon color="grey">{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item>
+        <v-list-item-content>
+          <v-list-item-subtitle>
+            TELLIMUSED
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+        <v-list-item
+          v-for="[avatar, name] in subscriptions"
+          :key="avatar"
+          link
+        >
+          <v-list-item-avatar size="25">
+          <v-img
+            :alt="`${name} avatar`"
+            :src="avatar"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="name"></v-list-item-title>
+        </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
+
+
+
+
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
-      dark
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
+        <v-icon>mdi-menu</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
@@ -80,12 +112,6 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -93,25 +119,36 @@
 export default {
   data () {
     return {
-      clipped: false,
+      clipped: true,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Youtube',
+      cards: ['Today', 'Yesterday'],
+      links_main: [
+        ['mdi-home', 'Avaleht'],
+        ['mdi-fire', 'Populaarsed'],
+        ['mdi-youtube-subscription', 'Tellimused'],
+      ],
+      links_secondary: [
+        ['mdi-play-box-multiple', 'Kogu'],
+        ['mdi-history', 'Ajalugu'],
+        ['mdi-play-box-outline', 'Teie videod'],
+        ['mdi-clock', 'Hiljem vaatamiseks'],
+        ['mdi-playlist-play', 'Lemmikud'],
+        ['mdi-thumb-up', 'Meeldinud videod'],
+      ],
+      subscriptions: [
+        ['https://yt3.ggpht.com/ytc/AAUvwniGuwmwnYd63hDKeebvj4KBUBvouY3FtszqSRMrYw=s88-c-k-c0x00ffffff-no-rj', 'Sidemen'],
+        ['https://yt3.ggpht.com/ytc/AAUvwninLx6_ZEJLBDLKYaB-9KMKLb5aQOoFUC_cx-Kz=s88-c-k-c0x00ffffff-no-rj', 'SidemenReacts'],
+        ['https://yt3.ggpht.com/ytc/AAUvwniVxsVsLQpT-pOCm1CpW1sG5PkFRBbAe4jExlSClg=s88-c-k-c0x00ffffff-no-rj', 'MoreSidemen'],
+        ['https://yt3.ggpht.com/ytc/AAUvwnjbH0uVPRm82votoF1QMjuJbudQvTJjrR_cBOYyog=s88-c-k-c0x00ffffff-no-rj', 'SwaggerSouls'],
+        ['https://yt3.ggpht.com/ytc/AAUvwnipIOGF5oWVbSffcVHhvJm5guRr4A21wbbmy1NX=s88-c-k-c0x00ffffff-no-rj', 'LEMMiNO'],
+        ['https://yt3.ggpht.com/ytc/AAUvwnj4Nx2WoQcBF-mPsUhE8H4Xvvasx17DwhJ14MzQ3Q=s88-c-k-c0x00ffffff-no-rj', 'Pyrocynical'],
+        ['https://yt3.ggpht.com/ytc/AAUvwnhKr-KpJ_6VZDp1UHjpfnEjIQYQnxKQNMUg3WCMiw=s88-c-k-c0x00ffffff-no-rj', 'Callux'],
+      ]
     }
   }
 }
